2f3cdb00394460c5f122b615f17cba6dc36d7e16
Britney <Wu.Britney@gmail.com>
2024-01-11 18:15:06 -0500

    configured pricing page routing

--- cryptoSite/views.py
+++ cryptoSite/views.py
@@ -1,12 +1,20 @@
 from django.shortcuts import render
+import requests
+import json
 
 def home(request):  
-    import requests
-    import json
-    
-    price_request= requests.get('https://min-api.cryptocompare.com/data/pricemultifull?fsyms=BTC,ETH,XRP,BCH,EOS,:TC&tsyms=USD')
-    price=json.loads(price_request.content)
     api_request= requests.get('https://min-api.cryptocompare.com/data/v2/news/?lang=EN')
-    api=json.loads(api_request.content)
-    return render(request, 'home.html',{'api':api,'price':price})
+    
+    api = json.loads(api_request.content)
+
+    return render(request, 'home.html', {'api':api})
+
+def pricing(request):
+    price_request = requests.get('https://min-api.cryptocompare.com/data/pricemultifull?fsyms=BTC,ETH,XRP,BCH,EOS,:TC&tsyms=USD')
+    
+    price = json.loads(price_request.content)
+
+    return render(request, 'pricing.html', {'price':price})
+
+
 
